587886827d2f005121b8170e91e102335f845801
STEPS0N <stepan.klimoff@mail.ru>
2026-04-04 14:08:47 +0500

    Разработанный класс подключения к БД. Климов

---
+++ ReportGeneration_Klimov/Classes/Common/DbConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Storage;
+using ReportGeneration_Klimov.Models;
+
+namespace ReportGeneration_Klimov.Classes.Common
+{
+    public class DbConnection : DbContext
+    {
+        public DbSet<Discipline> Disciplines { get; set; }
+        public DbSet<Evaluation> Evaluations { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Work> Works { get; set; }
+
+        public DbConnection()
+        {
+            try
+            {
+                Database.EnsureCreated();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;port=3307;database=Stud;uid=root;pwd=;", new ServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
