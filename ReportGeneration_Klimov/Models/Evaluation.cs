96e11d82e154ce5207b127bdc94ef230bfd6fc80
STEPS0N <stepan.klimoff@mail.ru>
2026-04-04 13:56:38 +0500

    Разработанные модели проекта. Климов

---
+++ ReportGeneration_Klimov/Models/Evaluation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGeneration_Klimov.Models
+{
+    public class Evaluation
+    {
+        [Key]
+        public int Id { get; set; }
+        public int IdWork { get; set; }
+        public int IdStudent { get; set; }
+        public string Value { get; set; }
+        public string Lateness { get; set; }
+    }
+}
---
+++ ReportGeneration_Klimov/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGeneration_Klimov.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int IdGroup { get; set; }
+        public bool Expelled { get; set; }
+        public DateTime DateExpelled { get; set; }
+    }
+}
