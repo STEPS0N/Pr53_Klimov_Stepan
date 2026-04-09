0112f196e95aff185598f685fa9eea63bb9e780a
STEPS0N <stepan.klimoff@mail.ru>
2026-04-09 12:41:19 +0500

    Программирование окна MainWindow. Климов

--- ReportGeneration_Klimov/MainWindow.xaml.cs
+++ ReportGeneration_Klimov/MainWindow.xaml.cs
@@ -20,9 +20,16 @@ namespace ReportGeneration_Klimov
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+
+            OpenPage(new Pages.Main());
         }
+
+        public void OpenPage(Page page) =>
+            frame.Navigate(page);
     }
 }
